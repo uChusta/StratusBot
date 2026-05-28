7886aacbd0aaf3e8c9e61787f9091026bafc99b1
uChusta <118682761+uChusta@users.noreply.github.com>
2026-05-28 21:51:11 +0200

    Add project files.

---
+++ Sound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+    internal class Sound
+    {
+        public void PlaySound()
+        {
+            try
+            {
+                using var audio = new AudioFileReader("Welcome.wav") { Volume = 0.25f }; // 25% volume
+                using var output = new WaveOutEvent();
+                output.Init(audio);
+                output.Play();
+                while (output.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            catch
+            {
+                // fail silently if audio unavailable
+            }
+        }
+    }
